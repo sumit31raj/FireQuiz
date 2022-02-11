9007cf71f033fb98b9de026a4d443fd76ed67b14
sumit31raj <sumit31raj@gmail.com>
2022-02-11 16:51:44 +0530

    Setup theme & size for scaling view based on device dimension

---
+++ src/theme/Colors.ts
@@ -0,0 +1,14 @@
+const Colors = {
+  background: {
+    primary: '#3b6d82',
+    secondary: '#75d9fe',
+    transparent: 'transparent',
+  },
+  button: {
+    primary: '#3b6d82',
+    seecondary: '#6391a6',
+  },
+  white: '#ffffff',
+};
+
+export { Colors };
